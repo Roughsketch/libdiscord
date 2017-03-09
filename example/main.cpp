249b86b8c8894463a1128e4786b6f82509af8925
Maiddog <maiddogsrl@gmail.com>
2017-03-08 20:18:14 -0600

    Add memory check command.

--- example/main.cpp
+++ example/main.cpp
@@ -1,5 +1,6 @@
 #include <discord.h>
 
+#include "getRSS.h"
 #include "json.hpp"
 
 nlohmann::json read_json_file(std::string file)
@@ -31,6 +32,13 @@ int main()
   bot.add_command("info", [&bot](auto& event)
   {
     event.respond("I am " + bot.profile().distinct() + "(" + bot.profile().id().to_string() + ")");
+  });
+
+  bot.add_command("mem", [&bot](auto& event)
+  {
+    auto current_mb = std::round(RSS::current() / 1000.0) / 1000.0;
+    auto peak_mb = std::round(RSS::peak() / 1000.0) / 1000.0;
+    event.respond("```Current memory usage: " + std::to_string(current_mb) + "MB\nPeak memory usage:    " + std::to_string(peak_mb) + "MB```");
   });
 
   bot.run();
