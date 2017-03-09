10312f7f48446835dceb61820eead5b05e85f8a3
Maiddog <maiddogsrl@gmail.com>
2017-03-08 20:07:04 -0600

    Add new event include.

--- libdiscord/include/discord.h
+++ libdiscord/include/discord.h
@@ -3,6 +3,8 @@
 #include "bot.h"
 #include "common.h"
 #include "channel.h"
+#include "discord_exception.h"
+#include "event/message_event.h"
 #include "guild.h"
 #include "member.h"
 #include "role.h"
