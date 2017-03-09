3a1c2c85b3b54f1fa6304ae33e7080038b3088bb
Maiddog <maiddogsrl@gmail.com>
2017-03-08 20:05:36 -0600

    Add json to string method to avoid using StringBuffer/Writer everywhere it's needed.

--- libdiscord/src/common.cpp
+++ libdiscord/src/common.cpp
@@ -1,3 +1,24 @@
 #include "common.h"
 
 INITIALIZE_EASYLOGGINGPP
+
+namespace discord
+{
+  std::string json_to_string(rapidjson::Value& data)
+  {
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    data.Accept(writer);
+
+    return buffer.GetString();
+  }
+
+  std::string json_to_string(const rapidjson::Value& data)
+  {
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    data.Accept(writer);
+
+    return buffer.GetString();
+  }
+}
