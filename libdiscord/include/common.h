cc75ab3e3a65747347578ef794ba8a9d65144acc
Maiddog <maiddogsrl@gmail.com>
2017-03-08 20:06:42 -0600

    Add template that assigns value only if json has the given key and it's not null.

--- libdiscord/include/common.h
+++ libdiscord/include/common.h
@@ -8,3 +8,39 @@
 
 #include "identifiable.h"
 #include "snowflake.h"
+
+namespace discord
+{
+  std::string json_to_string(rapidjson::Value& data);
+  std::string json_to_string(const rapidjson::Value& data);
+
+  template <typename Type, typename Key>
+  void set_from_json(Type& value, Key key, rapidjson::Value& data)
+  {
+    auto iter = data.FindMember(key);
+
+    if (iter == data.MemberEnd() || iter->value.IsNull())
+    {
+      value = Type{};
+    }
+    else
+    {
+      value = iter->value.template Get<Type>();
+    }
+  }
+
+  template <typename Key>
+  void set_from_json(snowflake& value, Key key, rapidjson::Value& data)
+  {
+    auto iter = data.FindMember(key);
+
+    if (iter == data.MemberEnd() || iter->value.IsNull())
+    {
+      value = snowflake{};
+    }
+    else
+    {
+      value = snowflake(iter->value.GetString());
+    }
+  }
+}
